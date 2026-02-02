56b65009f4ce748221b50ae72a5090bf070d27f1
Arisaya <117544545+ArisayaDragon@users.noreply.github.com>
2026-02-01 20:57:18 -0800

    Chore: syntax cleanup

--- transcendence_effect_placer/ui/sprite_settings.py
+++ transcendence_effect_placer/ui/sprite_settings.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import tkinter as tk
-from tkinter import LEFT, RIGHT, TOP, BOTTOM, X, Y, VERTICAL, HORIZONTAL, BOTH, Toplevel, Tk
+from tkinter import LEFT, RIGHT, TOP, BOTTOM, X, Y, VERTICAL, HORIZONTAL, BOTH, END, Toplevel, Tk, Label, Entry, Button
 
 from transcendence_effect_placer.data.data import SpriteConfig
     
@@ -16,84 +16,84 @@ class SpriteSettingsDialogue:
     def open_dialogue(self, defaults: SpriteConfig|None = None):
         defaults = self._sprite_cfg if defaults is None else defaults
 
-        self._wnd = tk.Toplevel()
+        self._wnd = Toplevel()
         self._wnd.title("Sprite Settings")
 
         c = 0
 
-        tk.Label(self._wnd, text="Sprite Pos X").grid(row=c, column=0)
-        self.sprite_pos_x_entry = tk.Entry(self._wnd)
-        self.sprite_pos_x_entry.insert(tk.END, str(defaults.x))
+        Label(self._wnd, text="Sprite Pos X").grid(row=c, column=0)
+        self.sprite_pos_x_entry = Entry(self._wnd)
+        self.sprite_pos_x_entry.insert(END, str(defaults.x))
         self.sprite_pos_x_entry.grid(row=c, column=1)
         c += 1
 
-        tk.Label(self._wnd, text="Sprite Pos Y").grid(row=c, column=0)
-        self.sprite_pos_y_entry = tk.Entry(self._wnd)
-        self.sprite_pos_y_entry.insert(tk.END, str(defaults.y))
+        Label(self._wnd, text="Sprite Pos Y").grid(row=c, column=0)
+        self.sprite_pos_y_entry = Entry(self._wnd)
+        self.sprite_pos_y_entry.insert(END, str(defaults.y))
         self.sprite_pos_y_entry.grid(row=c, column=1)
         c += 1
 
-        tk.Label(self._wnd, text="Sprite Width").grid(row=c, column=0)
-        self.sprite_width_entry = tk.Entry(self._wnd)
-        self.sprite_width_entry.insert(tk.END, str(defaults.w))
+        Label(self._wnd, text="Sprite Width").grid(row=c, column=0)
+        self.sprite_width_entry = Entry(self._wnd)
+        self.sprite_width_entry.insert(END, str(defaults.w))
         self.sprite_width_entry.grid(row=c, column=1)
         c += 1
 
-        tk.Label(self._wnd, text="Sprite Height").grid(row=c, column=0)
-        self.sprite_height_entry = tk.Entry(self._wnd)
-        self.sprite_height_entry.insert(tk.END, str(defaults.h))
+        Label(self._wnd, text="Sprite Height").grid(row=c, column=0)
+        self.sprite_height_entry = Entry(self._wnd)
+        self.sprite_height_entry.insert(END, str(defaults.h))
         self.sprite_height_entry.grid(row=c, column=1)
         c += 1
 
-        tk.Label(self._wnd, text="Animation Frames").grid(row=c, column=0)
-        self.animation_frames_entry = tk.Entry(self._wnd)
-        self.animation_frames_entry.insert(tk.END, str(defaults.anim_frames))
+        Label(self._wnd, text="Animation Frames").grid(row=c, column=0)
+        self.animation_frames_entry = Entry(self._wnd)
+        self.animation_frames_entry.insert(END, str(defaults.anim_frames))
         self.animation_frames_entry.grid(row=c, column=1)
         c += 1
 
-        tk.Label(self._wnd, text="Rotation Frames").grid(row=c, column=0)
-        self.rotation_frames_entry = tk.Entry(self._wnd)
-        self.rotation_frames_entry.insert(tk.END, str(defaults.rot_frames))
+        Label(self._wnd, text="Rotation Frames").grid(row=c, column=0)
+        self.rotation_frames_entry = Entry(self._wnd)
+        self.rotation_frames_entry.insert(END, str(defaults.rot_frames))
         self.rotation_frames_entry.grid(row=c, column=1)
         c += 1
 
-        tk.Label(self._wnd, text="Rotation Columns").grid(row=c, column=0)
-        self.rotation_columns_entry = tk.Entry(self._wnd)
-        self.rotation_columns_entry.insert(tk.END, str(defaults.rot_cols))
+        Label(self._wnd, text="Rotation Columns").grid(row=c, column=0)
+        self.rotation_columns_entry = Entry(self._wnd)
+        self.rotation_columns_entry.insert(END, str(defaults.rot_cols))
         self.rotation_columns_entry.grid(row=c, column=1)
         c += 1
 
-        tk.Label(self._wnd, text="Viewport Ratio").grid(row=c, column=0)
-        self.viewport_ratio_entry = tk.Entry(self._wnd)
-        self.viewport_ratio_entry.insert(tk.END, str(defaults.viewport_ratio))
+        Label(self._wnd, text="Viewport Ratio").grid(row=c, column=0)
+        self.viewport_ratio_entry = Entry(self._wnd)
+        self.viewport_ratio_entry.insert(END, str(defaults.viewport_ratio))
         self.viewport_ratio_entry.grid(row=c, column=1)
         c += 1
 
-        def accept():
-            self._sprite_cfg.x = int(self.sprite_pos_x_entry.get())
-            self._sprite_cfg.y = int(self.sprite_pos_y_entry.get())
-            self._sprite_cfg.w = int(self.sprite_width_entry.get())
-            self._sprite_cfg.h = int(self.sprite_height_entry.get())
-            self._sprite_cfg.anim_frames = int(self.animation_frames_entry.get())
-            self._sprite_cfg.rot_frames = int(self.rotation_frames_entry.get())
-            self._sprite_cfg.rot_cols = int(self.rotation_columns_entry.get())
-            self._sprite_cfg.viewport_ratio = float(self.viewport_ratio_entry.get())
-            self._sprite_cfg.real = True
-
-            if self._wnd is None: return
-            self._wnd.destroy()
-            self._wnd = None
-
-        def cancel():
-            if self._wnd is None: return
-            self._wnd.destroy()
-            self._wnd = None
-
-        self.accept_button = tk.Button(self._wnd, text="Accept", command=accept)
+        self.accept_button = Button(self._wnd, text="Accept", command=self.accept)
         self.accept_button.grid(row=c, column=0, columnspan=2)
         c += 1
 
-        self.accept_button = tk.Button(self._wnd, text="Cancel", command=cancel)
+        self.accept_button = Button(self._wnd, text="Cancel", command=self.cancel)
         self.accept_button.grid(row=c, column=0, columnspan=2)
 
         self._root.wait_window(self._wnd)
+
+    def accept(self):
+        self._sprite_cfg.x = int(self.sprite_pos_x_entry.get())
+        self._sprite_cfg.y = int(self.sprite_pos_y_entry.get())
+        self._sprite_cfg.w = int(self.sprite_width_entry.get())
+        self._sprite_cfg.h = int(self.sprite_height_entry.get())
+        self._sprite_cfg.anim_frames = int(self.animation_frames_entry.get())
+        self._sprite_cfg.rot_frames = int(self.rotation_frames_entry.get())
+        self._sprite_cfg.rot_cols = int(self.rotation_columns_entry.get())
+        self._sprite_cfg.viewport_ratio = float(self.viewport_ratio_entry.get())
+        self._sprite_cfg.real = True
+
+        if self._wnd is None: return
+        self._wnd.destroy()
+        self._wnd = None
+
+    def cancel(self):
+        if self._wnd is None: return
+        self._wnd.destroy()
+        self._wnd = None
